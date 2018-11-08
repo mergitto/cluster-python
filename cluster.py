f05887cbb48e7977e40f2d274f17e8080ca2b378
mergitto <merugitosu@gmail.ccom>
2018-11-08 18:36:55 +0900

    pickleデータを受け取りクラスタを引っ付けてpickleデータとして出力保存

--- cluster.py
+++ cluster.py
@@ -1,14 +1,22 @@
 # 分かち書きされた文書のクラスタリング
 
-import sys
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
-def wakati_to_docs(wakati_file_name):
-    df = pd.read_csv(wakati_file_name, header=None)
-    docs = np.array(df).reshape(-1,)
+def load_pickle():
+    import pickle
+    with open("./advice_2_tfidf.pickle", 'rb') as f:
+        return pickle.load(f)
+
+def dump_pickle(pickle_data, save_file_name):
+    import pickle
+    with open(save_file_name, 'wb') as f:
+        pickle.dump(pickle_data, f)
+
+def wakati_to_docs(wakati_documents):
+    docs = np.array(wakati_documents).reshape(-1,)
     return docs
 
 def get_vecs(docs):
@@ -16,8 +24,12 @@ def get_vecs(docs):
     vecs = vectorizer.fit_transform(docs)
     return vecs
 
-def get_docs_cluster(wakati_file_name, n_clusters):
-    docs = wakati_to_docs(wakati_file_name)
+def pluck_wakati_space_advice(reports):
+    return [report["advice_divide_mecab_space"] for report in reports.values()]
+
+def get_docs_cluster(reports, n_clusters):
+    wakati_documents = pluck_wakati_space_advice(reports)
+    docs = wakati_to_docs(wakati_documents)
     vecs = get_vecs(docs)
     clusters = KMeans(n_clusters=n_clusters, random_state=0).fit_predict(vecs)
     return docs, clusters
@@ -26,14 +38,21 @@ def show_cluster_doc(docs, clusters):
     for doc, cls in zip(docs, clusters):
         print(cls, doc)
 
-def write_csv(docs, clusters):
+def write_csv(docs, clusters, save_file_name):
     pd.DataFrame({
         'cluster_number': clusters,
         'documents': docs,
-    }).to_csv(sys.argv[3], index=None)
+    }).to_csv(save_file_name, index=None)
 
 if __name__ == '__main__':
-    n_clusters = int(sys.argv[1])
-    docs, clusters = get_docs_cluster(sys.argv[2], n_clusters)
-    write_csv(docs, clusters)
+    n_clusters = 10
+    reports = load_pickle()
+    docs, clusters = get_docs_cluster(reports, n_clusters)
+
+    for index in reports:
+        reports[index]["cluster"] = clusters[index]
+
+    dump_pickle(reports, "advice_2_cluster.pickle")
+
+    #write_csv(docs, clusters, "cluster.csv")
     #show_cluster_doc(docs, clusters)
